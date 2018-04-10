3e226231a34c48049b2add5fef93d0ee899d74ce
Omar <ocontreras.sf@gmail.com>
2018-04-10 16:32:21 +0000

    Account for 0, use functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 import random
 
-number = random.randint(-10000, 10000)
-last_digit = 0
 
-def get_last_nd(number):
-    return -10 + (number % 10)
+def get_randn():
+    return random.randint(-10000, 10000)
 
-if number > 0:
-    last_digit = number % 10
-else:
-    last_digit = get_last_nd(number)
 
-print('Last digit of {:d} is {:d}'.format(number, last_digit), end=" ")
-if last_digit > 5:
-    print('and is greater than 5')
-elif last_digit == 0:
-    print('and is 0')
-elif last_digit < 6 and last_digit != 0:
-    print('and is less than 6 and not 0')
+def get_last_digit(number):
+    if number >= 0:
+        return number % 10
+    else:
+        return -10 + (number % 10)
+
+
+def print_last_digit(last_digit):
+    print('Last digit of {:d} is {:d}'.format(number, last_digit), end=" ")
+    if last_digit > 5:
+        print('and is greater than 5')
+    elif last_digit == 0:
+        print('and is 0')
+    elif last_digit < 6 and last_digit != 0:
+        print('and is less than 6 and not 0')
+
+
+if __name__ == '__main__':
+    number = get_randn()
+    last_digit = get_last_digit(number)
+    print_last_digit(last_digit)
