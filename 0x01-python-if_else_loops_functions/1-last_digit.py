5a1e865d1879f93b9555c9eeeac0c05ceb773c6a
Omar <ocontreras.sf@gmail.com>
2018-04-10 22:58:14 +0000

    Use format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -16,11 +16,11 @@ def get_last_digit(number):
 def print_last_digit(last_digit):
     print('Last digit of {:d} is {:d}'.format(number, last_digit), end=" ")
     if last_digit > 5:
-        print('and is greater than 5')
+        print('{}'.format('and is greater than 5'))
     elif last_digit == 0:
-        print('and is 0')
+        print('{}'.format('and is 0'))
     elif last_digit < 6 and last_digit != 0:
-        print('and is less than 6 and not 0')
+        print('{}'.format('and is less than 6 and not 0'))
 
 
 if __name__ == '__main__':
