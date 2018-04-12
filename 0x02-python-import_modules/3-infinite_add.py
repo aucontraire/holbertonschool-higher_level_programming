6d9527d1db025888dab74562bdf147e49085533d
Omar <ocontreras.sf@gmail.com>
2018-04-12 19:38:26 +0000

    Move imports to if

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
 
 
 if __name__ == '__main__':
-    sum = 0
+    from sys import argv
 
+    sum = 0
     for i, arg in enumerate(argv[1:]):
         sum += int(arg)
 
