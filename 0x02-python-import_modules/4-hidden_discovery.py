929b5e62835cb964ff78912f5ba7a6c31cc6c181
Omar <ocontreras.sf@gmail.com>
2018-04-12 19:36:24 +0000

    Move imports to if

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
+
 
 if __name__ == '__main__':
+    import hidden_4
+
     name_list = dir(hidden_4)
     for st in name_list:
         if not st.startswith('__'):
