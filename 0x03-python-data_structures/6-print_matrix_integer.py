aec56130d6a8843b8c369ce64a946ede3fc2ba56
Omar <ocontreras.sf@gmail.com>
2018-04-17 20:41:04 +0000

    Account for None input

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,14 +2,15 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    rows = len(matrix)
-    columns = len(matrix[0])
-    for r in range(rows):
-        for c in range(columns):
-            if c == columns - 1:
-                print('{:d}'.format(matrix[r][c]))
-            else:
-                print('{:d} '.format(matrix[r][c]), end='')
+    if matrix is not None:
+        rows = len(matrix)
+        columns = len(matrix[0])
+        for r in range(rows):
+            for c in range(columns):
+                if c == columns - 1:
+                    print('{:d}'.format(matrix[r][c]))
+                else:
+                    print('{:d} '.format(matrix[r][c]), end='')
 
 
 if __name__ == '__main__':
