e15ef31456cfae89e5ae9e8071a1bca14d74aa62
Omar <ocontreras.sf@gmail.com>
2018-04-17 16:02:43 +0000

    Add function that prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    rows = len(matrix)
+    columns = len(matrix[0])
+    for r in range(rows):
+        for c in range(columns):
+            if c == columns - 1:
+                print('{:d}'.format(matrix[r][c]))
+            else:
+                print('{:d} '.format(matrix[r][c]), end='')
+
+
+if __name__ == '__main__':
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    print_matrix_integer(matrix)
--- 0x03-python-data_structures/README.md
+++ 0x03-python-data_structures/README.md
@@ -5,3 +5,4 @@
 * 3-print_reversed_list_integer.py: function that prints all integers of a list, in reverse order
 * 4-new_in_list.py: function that replaces an element in a list at a specific position without modifying the original list
 * 5-no_c.py: function that removes all characters c and C from a string
+* 6-print_matrix_integer.py: function that prints a matrix of integers
