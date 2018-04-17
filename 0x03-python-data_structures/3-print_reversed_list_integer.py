40b48dc2ca07cdb81209c4d27e1508b9e097d7f2
Omar <ocontreras.sf@gmail.com>
2018-04-17 20:38:19 +0000

    Account for None input

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,8 +2,9 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list) - 1, -1, -1):
-        print('{:d}'.format(my_list[i]))
+    if my_list is not None:
+        for i in range(len(my_list) - 1, -1, -1):
+            print('{:d}'.format(my_list[i]))
 
 
 if __name__ == '__main__':
