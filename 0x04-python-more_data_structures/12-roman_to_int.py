25b594acc0a44e1c36be49a2e8598c4fa06b31c9
Omar <ocontreras.sf@gmail.com>
2018-04-19 20:50:21 +0000

    Use and instead of or

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 
 def roman_to_int(roman_string):
-    if roman_string is not None or type(roman_string) == str:
+    if roman_string is not None and type(roman_string) == str:
         rom_dict = {
             'I': 1,
             'V': 5,
