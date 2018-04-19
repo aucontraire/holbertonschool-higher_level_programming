8ed18c5f3b9efe515e19ae285413b5b8b2d877c5
Omar <ocontreras.sf@gmail.com>
2018-04-19 16:34:02 +0000

    Add function that computes the square value of all integers of a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    rows = len(matrix)
+    columns = len(matrix[0])
+    if columns == 0:
+        return matrix
+    else:
+        sq_matrix = [[0 for i in range(rows)] for j in range(columns)]
+        for r in range(rows):
+            for c in range(columns):
+                sq_matrix[r][c] = matrix[r][c] ** 2
+
+        return sq_matrix
+
+
+if __name__ == '__main__':
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,2 @@
+# More Data Structures: Set, Dictionary
+* 0-square_matrix_simple.py: function that computes the square value of all integers of a matrix
