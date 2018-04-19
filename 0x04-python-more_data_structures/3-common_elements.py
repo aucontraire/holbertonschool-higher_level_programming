34f458af72acb134049292853d6ea08a0a3a8d54
Omar <ocontreras.sf@gmail.com>
2018-04-19 22:08:03 +0000

    Remove extra spaces

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -7,7 +7,7 @@ def common_elements(set_1, set_2):
 
 
 if __name__ == '__main__':
-    set_1 = { "Python", "C", "Javascript" }
-    set_2 = { "Bash", "C", "Ruby", "Perl" }
+    set_1 = {"Python", "C", "Javascript"}
+    set_2 = {"Bash", "C", "Ruby", "Perl"}
     c_set = common_elements(set_1, set_2)
     print(sorted(list(c_set)))
