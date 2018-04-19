7ef7339b576b844fd9c73136a57c7ba33e91f02c
Omar <ocontreras.sf@gmail.com>
2018-04-19 20:45:29 +0000

    Account for roman_string is None

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,26 +2,27 @@
 
 
 def roman_to_int(roman_string):
-    rom_dict = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
+    if roma_string is not None:
+        rom_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
-    total = 0
-    prev = 1001
-    for c in roman_string:
-        if rom_dict[c] > prev:
-            total += rom_dict[c]
-            total -= prev * 2
-        else:
-            total += rom_dict[c]
-        prev = rom_dict[c]
-    return total
+        total = 0
+        prev = 1001
+        for c in roman_string:
+            if rom_dict[c] > prev:
+                total += rom_dict[c]
+                total -= prev * 2
+            else:
+                total += rom_dict[c]
+                prev = rom_dict[c]
+        return total
 
 
 if __name__ == '__main__':
