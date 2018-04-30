9488be2ce5250263dfc21c3f60ff1d1d8d93b4ed
Omar <ocontreras.sf@gmail.com>
2018-04-30 20:45:45 +0000

    Use for loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,24 +2,23 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    while True:
+    count = 0
+    for i in range(x):
         try:
             print('{}'.format(my_list[i]), end='')
-            i += 1
-            if i == x:
-                if i != 0:
-                    print()
-                return i
+            count += 1
+        except TypeError:
+            break
         except IndexError:
-            if i != 0:
-                print()
-            return i
+            break
+    if count != 0:
+        print()
+    return count
 
 
 if __name__ == '__main__':
     my_list = [1, 2, 3, 4, 5]
-    #my_list = []
+
     nb_print = safe_print_list(my_list, 2)
     print("nb_print: {:d}".format(nb_print))
     nb_print = safe_print_list(my_list, len(my_list))
