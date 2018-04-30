dbac109d0d3b23bc9b66f862d740a21520bc1c99
Omar <ocontreras.sf@gmail.com>
2018-04-30 21:07:05 +0000

    Remove if __name__

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,12 +15,3 @@ def safe_print_list(my_list=[], x=0):
     return count
 
 
-if __name__ == '__main__':
-    my_list = [1, 2, 3, 4, 5]
-
-    nb_print = safe_print_list(my_list, 2)
-    print("nb_print: {:d}".format(nb_print))
-    nb_print = safe_print_list(my_list, len(my_list))
-    print("nb_print: {:d}".format(nb_print))
-    nb_print = safe_print_list(my_list, len(my_list) + 2)
-    print("nb_print: {:d}".format(nb_print))
