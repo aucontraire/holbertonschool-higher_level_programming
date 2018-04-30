ab19fbd38be91cd126a71912b8a2b5992d23e46d
Omar <ocontreras.sf@gmail.com>
2018-04-30 20:25:06 +0000

    Add function that divides element by element 2 lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+
+def safe_division(a, b):
+    quotient = 0
+
+    try:
+        quotient = a / b
+    except ZeroDivisionError:
+        print("division by 0")
+        quotient = 0
+    except TypeError:
+        print("wrong type")
+        quotient = 0
+    finally:
+        return quotient
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    result_li = []
+    for i in range(list_length):
+        try:
+            result_li.append(safe_division(my_list_1[i], my_list_2[i]))
+        except IndexError:
+            print("out of range")
+            result_li.append(0)
+
+    return result_li
+
+
+if __name__ == '__main__':
+    my_l_1 = [10, 8, 4]
+    my_l_2 = [2, 4, 4]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
+
+    print("--")
+
+    my_l_1 = [10, 8, 4, 4]
+    my_l_2 = [2, 0, "H", 2, 7]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -3,3 +3,4 @@
 * 1-safe_print_integer.py: function that prints an integer with "{:d}".format()
 * 2-safe_print_list_integers.py: function that prints the first x elements of a list and only integers
 * 3-safe_print_division.py: function that divides 2 integers and prints the result
+* 4-list_division.py: function that divides element by element 2 lists
