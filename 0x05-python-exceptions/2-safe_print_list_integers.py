bfc52394c8f3c792463f6784be083931ead5e015
Omar <ocontreras.sf@gmail.com>
2018-04-30 21:55:04 +0000

    Add if __name__ == '__main__' again

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -15,3 +15,16 @@ def safe_print_list_integers(my_list=[], x=0):
 
     print()
     return count
+
+if __name__ == '__main__':
+    my_list = [1, 2, 3, 4, 5]
+
+    nb_print = safe_print_list_integers(my_list, 2)
+    print("nb_print: {:d}".format(nb_print))
+
+    my_list = [1, 2, 3, "Holberton", 4, 5, [1, 2, 3]]
+    nb_print = safe_print_list_integers(my_list, len(my_list))
+    print("nb_print: {:d}".format(nb_print))
+
+    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+    print("nb_print: {:d}".format(nb_print))
