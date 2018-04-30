6579db889f9d276cede56b4f027458c92704a9d2
Omar <ocontreras.sf@gmail.com>
2018-04-30 20:58:10 +0000

    Use pass instead of break

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
             print('{}'.format(my_list[i]), end='')
             count += 1
         except IndexError:
-            break
+            pass
     if count != 0:
         print()
     return count
