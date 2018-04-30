b6517a38c454c69ad7c6eb6f50518612ee3db1d8
Omar <ocontreras.sf@gmail.com>
2018-04-30 23:43:00 +0000

    Add function that prints an integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print('{:d}'.format(value))
+        return True
+    except ValueError as err:
+        err = "Exception: " + str(err) + "\n"
+        sys.stderr.write(err)
+
+
+if __name__ == '__main__':
+    value = 89
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = -89
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = "Holberton"
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -4,3 +4,6 @@
 * 2-safe_print_list_integers.py: function that prints the first x elements of a list and only integers
 * 3-safe_print_division.py: function that divides 2 integers and prints the result
 * 4-list_division.py: function that divides element by element 2 lists
+* 5-raise_exception.py: function that raises a type exception
+* 6-raise_exception_msg.py: function that raises a name exception with a message
+* 100-safe_print_integer_err.py: function that prints an integer
