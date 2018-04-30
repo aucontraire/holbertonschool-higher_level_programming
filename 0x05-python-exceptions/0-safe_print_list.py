ad6405acabc97fd611b03c21a9e0bdb2ba9729d1
Omar <ocontreras.sf@gmail.com>
2018-04-30 21:03:51 +0000

    Test commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print('{}'.format(my_list[i]), end='')
             count += 1
+        except (ValueError, TypeError):
+            pass
         except IndexError:
             break:
     print()
