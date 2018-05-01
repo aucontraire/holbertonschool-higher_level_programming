548a1b08702bb56911180f0765e84e2873f3d14e
Omar <ocontreras.sf@gmail.com>
2018-05-01 21:49:03 +0000

    Add  class Square that defines a square by: (based on 2-square.py)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Square class."""
+
+    def __init__(self, size=0):
+        """__init__ method for Square.
+
+        Args:
+            size (int): size of Square.
+
+        """
+        self.set_size(size)
+
+    def set_size(self, size):
+        """set_size sets the size of the square.
+
+        Args:
+            size (int): size of Square
+
+        Raises:
+            TypeError: If `size` is not an integer.
+            ValueError: If `size` is less than 0.
+
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Gets the area of the Square.
+
+        Returns:
+            Area of squre
+        """
+        return self.__size * self.__size
+
+
+if __name__ == '__main__':
+    my_square_1 = Square(3)
+    print("Area: {}".format(my_square_1.area()))
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    my_square_2 = Square(5)
+    print("Area: {}".format(my_square_2.area()))
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -2,3 +2,4 @@
 * 0-square.py: empty class Square that defines a square
 * 1-square.py: class Square that defines a square by: (based on 0-square.py)
 * 2-square.py: class Square that defines a square by: (based on 1-square.py)
+* 3-square.py: class Square that defines a square by: (based on 2-square.py)
