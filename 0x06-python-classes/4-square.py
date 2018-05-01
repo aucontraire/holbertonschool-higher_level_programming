b025394ec9b2c8becaa135f42167ec9d4bb6e25f
Omar <ocontreras.sf@gmail.com>
2018-05-01 22:29:00 +0000

    Add class Square that defines a square by: (based on 3-square.py)

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Square class."""
+
+    def __init__(self, size=0):
+        """__init__ method that sets the size of square.
+        Args:
+            size (int): size of Square
+
+        """
+        self.__size = size
+
+    def area(self):
+        """Gets the area of the Square.
+
+        Returns:
+            Area of squre
+
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter  method that sets the size of square.
+        Args:
+            value (int): size of Square
+        Raises:
+            TypeError: If `value` is not an integer.
+            ValueError: If `value` is less than 0.
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+if __name__ == '__main__':
+    my_square = Square(89)
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    my_square.size = 3
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    try:
+        my_square.size = "5 feet"
+        print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+    except Exception as e:
+        print(e)
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -3,3 +3,4 @@
 * 1-square.py: class Square that defines a square by: (based on 0-square.py)
 * 2-square.py: class Square that defines a square by: (based on 1-square.py)
 * 3-square.py: class Square that defines a square by: (based on 2-square.py)
+* 4-square.py: class Square that defines a square by: (based on 3-square.py)
