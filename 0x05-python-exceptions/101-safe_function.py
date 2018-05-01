161c2fc518c59c81f87c28b4d2294a363966a2a1
Omar <ocontreras.sf@gmail.com>
2018-05-01 00:25:57 +0000

    Make function more generalizable

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,8 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        result = fct(args[0], args[1])
-        return result
+        return fct(*args)
     except Exception as err:
         err = "Exception: " + str(err) + "\n"
         sys.stderr.write(err)
