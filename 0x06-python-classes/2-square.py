c8ea928b28c97ff77ab16ae5f19cfef8beff52c3
Omar <ocontreras.sf@gmail.com>
2018-05-01 22:04:10 +0000

    Simplify __init__ method, remove set_size() method

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,16 +5,7 @@ class Square:
     """Square class."""
 
     def __init__(self, size=0):
-        """__init__ method for Square.
-
-        Args:
-            size (int): size of Square.
-
-        """
-        self.set_size(size)
-
-    def set_size(self, size):
-        """set_size sets the size of the square.
+        """__init__ method that sets the size of square.
 
         Args:
             size (int): size of Square
@@ -22,7 +13,6 @@ class Square:
         Raises:
             TypeError: If `size` is not an integer.
             ValueError: If `size` is less than 0.
-
 
         """
         if not isinstance(size, int):
