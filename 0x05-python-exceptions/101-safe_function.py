3192a3563be23c3c45c42f2d63530b7f57ed24dd
Omar <ocontreras.sf@gmail.com>
2018-05-01 00:22:21 +0000

    Remove if __name__ == '__main__'

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -10,24 +10,3 @@ def safe_function(fct, *args):
         err = "Exception: " + str(err) + "\n"
         sys.stderr.write(err)
         return None
-
-
-if __name__ == '__main__':
-    def my_div(a, b):
-        return a / b
-
-    result = safe_function(my_div, 10, 2)
-    print("result of my_div: {}".format(result))
-
-    result = safe_function(my_div, 10, 0)
-    print("result of my_div: {}".format(result))
-
-    def print_list(my_list, len):
-        i = 0
-        while i < len:
-            print(my_list[i])
-            i += 1
-        return len
-
-    result = safe_function(print_list, [1, 2, 3, 4], 10)
-    print("result of print_list: {}".format(result))
