03fef4c99c87a713f4fc99d48ec1d9fee465b308
Omar <ocontreras.sf@gmail.com>
2018-05-02 03:46:53 +0000

    Remove if __name__ == '__main__'

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -85,18 +85,3 @@ class Square:
                         print('#', end='')
                 print()
 
-if __name__ == '__main__':
-    my_square_1 = Square(3)
-    my_square_1.my_print()
-
-    print("--")
-
-    my_square_2 = Square(3, (1, 1))
-    my_square_2.my_print()
-
-    print("--")
-
-    my_square_3 = Square(3, (3, 0))
-    my_square_3.my_print()
-
-    print("--")
