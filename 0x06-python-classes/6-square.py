831511518d4fff6450473a11354575b73afc0576
Omar <ocontreras.sf@gmail.com>
2018-05-02 00:29:24 +0000

    Add class Square with private instance attribute: position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Square class."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """__init__ method that sets the size and position of square.
+        Args:
+            size (int): size of Square
+            position (tuple): poisition of Square
+
+        """
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """Gets the area of the Square.
+
+        Returns:
+            Area of squre
+
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter  method that sets the size of square.
+        Args:
+            value (int): size of Square
+        Raises:
+            TypeError: If `value` is not an integer.
+            ValueError: If `value` is less than 0.
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """position setter method that sets position of Square.
+        Args:
+            value (tuple): tuple of two positive integer coordinates
+        Raises:
+            TypeError: If `value` is not a tuple of two positive integers
+
+        """
+        if not isinstance(value, tuple) or (not isinstance(value[0], int) or not isinstance(value[1], int)) or (value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def my_print(self):
+        """Prints a # representation of square based on size."""
+
+        if self.__size == 0:
+            print()
+        else:
+            width = self.__size + self.__position[0]
+            height = self.__size + self.__position[1]
+            for h in range(height):
+                for w in range(width):
+                    if h < self.__position[1] or w < self.__position[0]:
+                        print(' ', end='')
+                    else:
+                        print('#', end='')
+                print()
+
+
+if __name__ == '__main__':
+    my_square_1 = Square(3)
+    my_square_1.my_print()
+
+    print("--")
+
+    my_square_2 = Square(3, (1, 1))
+    my_square_2.my_print()
+
+    print("--")
+
+    my_square_3 = Square(3, (3, 0))
+    my_square_3.my_print()
+
+    print("--")
+
+    my_square_4 = Square(2, (10, 10))
+    my_square_4.my_print()
+
+    print("---")
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -5,3 +5,4 @@
 * 3-square.py: class Square that defines a square by: (based on 2-square.py)
 * 4-square.py: class Square that defines a square by: (based on 3-square.py)
 * 5-square.py: public instance method: def my_print(self): that prints in stdout the square with the character #
+* 6-square.py: private instance attribute: position
