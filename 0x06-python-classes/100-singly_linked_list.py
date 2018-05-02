ad0e35f8a9acb220d1ed03f0110a0ecabeb8e25e
Omar <ocontreras.sf@gmail.com>
2018-05-02 20:38:30 +0000

    Account for empty list in printing

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,28 +1,7 @@
 #!/usr/bin/python3
 """Node and Singly Linked List.
 
-This module demonstrates documentation as specified by the `Google Python
-Style Guide`_. Docstrings may extend over multiple lines. Sections are created
-with a section header and a colon followed by a block of indented text.
-
-Example:
-    Examples can be given using either the ``Example`` or ``Examples``
-    sections. Sections support any reStructuredText formatting, including
-    literal blocks::
-
-        $ python example_google.py
-
-Section breaks are created by resuming unindented text. Section breaks
-are also implicitly created anytime a new section starts.
-
-Attributes:
-    module_level_variable1 (int): Module level variables may be documented in
-        either the ``Attributes`` section of the module docstring, or in an
-        inline docstring immediately following the variable.
-
-        Either form is acceptable, but the two should not be mixed. Choose
-        one convention to document module level variables and be consistent
-        with it.
+This module provides a Node and a Linked List Class which adds sorted nodes
 
 """
 
@@ -121,7 +100,9 @@ class SinglyLinkedList:
         """
         curr = self.__head
         llstr = ''
-        if curr.next_node is None:
+        if curr is None:
+            llstr = ''
+        elif curr.next_node is None:
             llstr += str(curr.data)
         else:
             while curr.next_node:
@@ -133,7 +114,7 @@ class SinglyLinkedList:
 
 if __name__ == '__main__':
     sll = SinglyLinkedList()
-
+    """
     sll.sorted_insert(2)
     sll.sorted_insert(5)
     sll.sorted_insert(3)
@@ -144,5 +125,5 @@ if __name__ == '__main__':
     sll.sorted_insert(4)
     sll.sorted_insert(5)
     sll.sorted_insert(12)
-    sll.sorted_insert(3)
+    sll.sorted_insert(3)"""
     print(sll)
