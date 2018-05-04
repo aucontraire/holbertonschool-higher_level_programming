914f0f3e3080f9ff5341dac5ae2e412729d9a320
Omar <ocontreras.sf@gmail.com>
2018-05-04 04:53:22 +0000

    Add documentation

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
+""" say_my_name
+
+Prints My name is `first_name` `last_name`
+If either argument is not a string it raises TypeError
+"""
+
 
 def say_my_name(first_name, last_name=""):
+    """ add_integer - prints first and last name
+
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
