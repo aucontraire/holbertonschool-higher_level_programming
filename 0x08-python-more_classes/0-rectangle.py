5db73ebfaac9405f1ed794b6373f89c280059244
Omar <ocontreras.sf@gmail.com>
2018-05-07 17:29:04 +0000

    Add empty class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+    """Rectangle class."""
+    pass
+
+
+if __name__ == '__main__':
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -1,1 +1,2 @@
 # Python - More Classes and Objects
+* 0-rectangle.py: empty class Rectangle
