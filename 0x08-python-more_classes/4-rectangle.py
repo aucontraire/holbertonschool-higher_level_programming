b9d0e41ac5edb1753e91d0cba081ac6be7d97c38
Omar <ocontreras.sf@gmail.com>
2018-05-07 20:34:36 +0000

    Add class Rectangle with __repr__ method

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+    """Rectangle class."""
+
+    def __init__(self, width=0, height=0):
+        """__init__ method.
+        Args:
+            width (int): integer width
+            height (int): integer height
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width: returns width
+        Args:
+            width (int): integer width
+        Returns:
+            rectangle width
+        Raises:
+            TypeError: if width is not an integer
+            ValueError: if width is less than 0
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """height: returns height
+        Args:
+            height (int): integer height
+        Returns:
+            rectangle height
+        Raises:
+            TypeError: if height is not an integer
+            ValueError: if height is less than 0
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def __str__(self):
+        """String representation (#) of rectangle.
+        Returns:
+            string rep of rectangle
+        """
+        string = ''
+        if self.width == 0 or self.height == 0:
+            return string
+        for r in range(self.height - 1):
+            for c in range(self.width):
+                string += '#'
+            string += '\n'
+        string += '#' * self.width
+        return string
+
+    def __repr__(self):
+        """String representation of Rectangle object"""
+        return 'Rectangle({:d}, {:d})'.format(self.width, self.height)
+
+    def area(self):
+        """Returns the area of the rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return self.width * 2 + self.height * 2
+
+
+if __name__ == '__main__':
+    my_rectangle = Rectangle(2, 4)
+    print(str(my_rectangle))
+    print("--")
+    print(my_rectangle)
+    print("--")
+    print(repr(my_rectangle))
+    print("--")
+    print(hex(id(my_rectangle)))
+    print("--")
+
+    # create new instance based on representation
+    new_rectangle = eval(repr(my_rectangle))
+    print(str(new_rectangle))
+    print("--")
+    print(new_rectangle)
+    print("--")
+    print(repr(new_rectangle))
+    print("--")
+    print(hex(id(new_rectangle)))
+    print("--")
+
+    print(new_rectangle is my_rectangle)
+    print(type(new_rectangle) is type(my_rectangle))
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -3,3 +3,4 @@
 * 1-rectangle.py: class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
 * 2-rectangle.py: class Rectangle with area and perimeter methods
 * 3-rectangle.py: class Rectangle with print capability / string representation
+* 4-rectangle.py: class Rectangle with __repr__ method
