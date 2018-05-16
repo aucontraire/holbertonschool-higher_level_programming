dd4f6b1fab41aa7ce64ad9fd78de8e88be743965
Omar <ocontreras.sf@gmail.com>
2018-05-16 04:13:17 +0000

    Add class BaseGeometry with integer_validator method

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry:
+    """BaseGeometry class with area method"""
+    def area(self):
+        """Area method not implemented"""
+        raise Exception("area is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer validator
+        Args:
+            name (str): name
+            value (int): integer value
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than 1
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 1:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+if __name__ == '__main__':
+    bg = BaseGeometry()
+
+    bg.integer_validator("my_int", 12)
+    bg.integer_validator("width", 89)
+
+    try:
+        bg.integer_validator("name", "John")
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        bg.integer_validator("age", 0)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        bg.integer_validator("distance", -4)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -6,3 +6,4 @@
 * 4-inherits_from.py: function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class
 * 5-base_geometry.py: empty class BaseGeometry
 * 6-base_geometry.py: a class BaseGeometry with area method
+* 7-base_geometry.py, tests/7-base_geometry.txt: a class BaseGeometry with integer validator method
