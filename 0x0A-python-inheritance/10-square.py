4887b8aac96cf9692c7ebf02851a0bab9b50e53f
Omar <ocontreras.sf@gmail.com>
2018-05-16 19:12:00 +0000

    Add import Rectangle class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,49 +1,7 @@
 #!/usr/bin/python3
 
 
-class BaseGeometry:
-    """BaseGeometry class with area method"""
-    def area(self):
-        """Area method not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """integer validator
-        Args:
-            name (str): name
-            value (int): integer value
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than 1
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value < 1:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle class"""
-
-    def __init__(self, width, height):
-        """__init__ method
-        Args:
-            width (int): width integer must be greater than 0
-            height (int): height integer must be greater than 0
-        """
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def __str__(self):
-        """string representation"""
-        return "[{}] {:d}/{:d}".format(
-            type(self).__name__, self.__width, self.__height)
-
-    def area(self):
-        """Area of rectangle"""
-        return self.__width * self.__height
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -68,3 +26,4 @@ if __name__ == '__main__':
 
     print(s)
     print(s.area())
+    print(issubclass(Square, Rectangle))
