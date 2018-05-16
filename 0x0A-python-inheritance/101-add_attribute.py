7fea6d9a44424b7db8a1a7d07c28f23617c28992
Omar <ocontreras.sf@gmail.com>
2018-05-16 18:35:57 +0000

    Remove testing code (if __name__ == '__main__')

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -14,19 +14,3 @@ def add_attribute(obj, attr, value):
         setattr(obj, attr, value)
     else:
         raise TypeError("can't add new attribute")
-
-
-if __name__ == '__main__':
-    class MyClass():
-        pass
-
-    mc = MyClass()
-    add_attribute(mc, "name", "John")
-    print(mc.name)
-
-    try:
-        a = "My String"
-        add_attribute(a, "name", "Bob")
-        print(a.name)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
