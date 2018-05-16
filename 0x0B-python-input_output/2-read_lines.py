0b129e8dedd6a0df1355a4621c96a808e094b216
Omar <ocontreras.sf@gmail.com>
2018-05-16 20:36:22 +0000

    Add function that reads n lines of a text file and prints it to stdout

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+def read_lines(filename="", nb_lines=0):
+    """Count number of lines in file
+    Args:
+        filename (str): string of path to file
+    Returns:
+        number of lines in file
+
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        nlines = len(lines)
+        if nb_lines <= 0 or nb_lines >= nlines:
+            for line in lines:
+                print(line, end='')
+        else:
+            i = 0
+            while i < nb_lines:
+                print(lines[i], end='')
+                i += 1
+
+
+if __name__ == '__main__':
+    print("1 line:")
+    read_lines("my_file_0.txt", 1)
+    print("--")
+    print("3 lines:")
+    read_lines("my_file_0.txt", 3)
+    print("--")
+    print("Full content:")
+    read_lines("my_file_0.txt")
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -1,3 +1,4 @@
 # Python - Input/Output
 * 0-read_file.py: function that reads a text file (UTF8) and prints it to stdout
 * 1-number_of_lines.py: function that returns the number of lines of a text file
+* 2-read_lines.py: function that reads n lines of a text file (utf-8) and prints it to stdout
