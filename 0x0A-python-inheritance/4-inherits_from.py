8f6c0027cf0ec43cedcb33c7df3d2dec9bbed922
Omar <ocontreras.sf@gmail.com>
2018-05-16 18:50:38 +0000

    Remove test code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,13 +13,3 @@ def inherits_from(obj, a_class):
         if type(obj) is not a_class:
             return True
     return False
-
-
-if __name__ == '__main__':
-    a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
