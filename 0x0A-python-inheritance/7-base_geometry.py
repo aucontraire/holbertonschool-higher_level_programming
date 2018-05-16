7804a0b5606ac673d1f7548598d3567d33efbb5e
Omar <ocontreras.sf@gmail.com>
2018-05-16 19:01:30 +0000

    Remove test code

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,25 +20,3 @@ class BaseGeometry:
             raise TypeError("{:s} must be an integer".format(name))
         if value < 1:
             raise ValueError("{:s} must be greater than 0".format(name))
-
-
-if __name__ == '__main__':
-    bg = BaseGeometry()
-
-    bg.integer_validator("my_int", 12)
-    bg.integer_validator("width", 89)
-
-    try:
-        bg.integer_validator("name", "John")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("age", 0)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("distance", -4)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
