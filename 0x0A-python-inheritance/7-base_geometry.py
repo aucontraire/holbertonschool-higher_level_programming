a44228564952724fe32d16ec867d92e31c049370
Omar <ocontreras.sf@gmail.com>
2018-05-17 21:23:20 +0000

    Change zero check

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -18,5 +18,13 @@ class BaseGeometry:
         """
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
-        if value < 1:
+        if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
+
+if __name__ == '__main__':
+    bg = BaseGeometry()
+
+    bg.integer_validator("my_int", 12)
+    bg.integer_validator("width", 89)
+
+    bg.area()
