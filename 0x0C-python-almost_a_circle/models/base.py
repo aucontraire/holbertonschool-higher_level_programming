6b3ce34c38ddd1c0af66e304e9d0ae77e1ab1b93
Omar <ocontreras.sf@gmail.com>
2018-05-22 17:57:25 +0000

    Add class Base with save_to_file method and unittests

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,6 +19,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """JSON string of object dictionary
         Args:
@@ -27,3 +28,17 @@ class Base:
             JSON string representation of list of dictionary
         """
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        json_list = []
+        json_string = '[]'
+        if list_objs is not None:
+            for obj in list_objs:
+                json_list.append(obj.to_dictionary())
+
+            if len(json_list) > 0:
+                json_string = Base.to_json_string(json_list)
+
+        with open(cls.__name__ + '.json', 'w') as f:
+            f.write(json_string)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
@@ -37,6 +38,42 @@ class TestBaseClass(unittest.TestCase):
         self.assertIsInstance(sd, dict)
         json_sd = Base.to_json_string([sd])
         self.assertIsInstance(json_sd, str)
+
+    def test_save_to_file(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        dict_list = []
+        with open('Rectangle.json', 'r', encoding='utf-8') as f:
+            dict_list = json.load(f)
+        self.assertIsInstance(dict_list, list)
+        self.assertEqual(len(dict_list), 2)
+        list_ref = [
+            {'id': 6, 'height': 7, 'x': 2, 'width': 10, 'y': 8},
+            {'id': 7, 'height': 4, 'x': 0, 'width': 2, 'y': 0}
+        ]
+        self.assertListEqual(dict_list, list_ref)
+
+        Square.save_to_file([])
+        dict_list = []
+        with open('Square.json', 'r', encoding='utf-8') as f:
+            dict_list = json.load(f)
+        self.assertIsInstance(dict_list, list)
+        self.assertEqual(len(dict_list), 0)
+
+        s1 = Square(2, x=1, y=2, id=98)
+        s2 = Square(3, x=5, y=7, id=99)
+        Square.save_to_file([s1, s2])
+        dict_list = []
+        with open('Square.json', 'r', encoding='utf-8') as f:
+            dict_list = json.load(f)
+        self.assertIsInstance(dict_list, list)
+        self.assertEqual(len(dict_list), 2)
+        list_ref = [
+            {'x': 1, 'id': 98, 'size': 2, 'y': 2},
+            {'x': 5, 'id': 99, 'size': 3, 'y': 7}
+        ]
+        self.assertListEqual(dict_list, list_ref)
 
 
 if __name__ == '__main__':
