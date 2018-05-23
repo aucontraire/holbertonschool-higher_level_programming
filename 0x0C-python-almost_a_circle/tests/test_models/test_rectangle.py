779e599151d54e9ea2e7837ccf62ddfc2ecbd6db
Omar <ocontreras.sf@gmail.com>
2018-05-23 05:19:34 +0000

    Add docstrings to test_rectangle unittests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,19 +9,22 @@ from models.rectangle import Rectangle
 class TestRectangleClass(unittest.TestCase):
 
     def test_class_membership(self):
+        """Rectangle class unittest"""
         r0 = Rectangle(10, 2)
         self.assertIsInstance(r0, Base)
         self.assertIsInstance(r0, Rectangle)
 
     def test_attributes_with_correct_initialization(self):
+        """Rectangle  attribute unittest"""
         r1 = Rectangle(5, 6)
-        self.assertEqual(r1.id, 11)
+        self.assertEqual(r1.id, 12)
         self.assertEqual(r1.width, 5)
         self.assertEqual(r1.height, 6)
         self.assertEqual(r1.x, 0)
         self.assertEqual(r1.y, 0)
 
     def test_private_instance_attributes(self):
+        """Rectangle private attribute unittest"""
         r2 = Rectangle(7, 8)
         with self.assertRaises(AttributeError):
             r2.__width
@@ -33,6 +36,7 @@ class TestRectangleClass(unittest.TestCase):
             r2.__y
 
     def test_attributes_wrong_data_types(self):
+        """Rectangle wrong data types unittest"""
         with self.assertRaises(TypeError):
             r3 = Rectangle('a', 9)
         with self.assertRaises(TypeError):
@@ -43,6 +47,7 @@ class TestRectangleClass(unittest.TestCase):
             r6 = Rectangle(9, 9, 3, 'a')
 
     def test_attributes_with_wrong_int_range(self):
+        """Rectangle wrong int range unittest"""
         with self.assertRaises(ValueError):
             r7 = Rectangle(0, 1)
         with self.assertRaises(ValueError):
@@ -53,10 +58,12 @@ class TestRectangleClass(unittest.TestCase):
             r10 = Rectangle(1, 1, 1, -2)
 
     def test_area_method(self):
+        """Rectangle method unittest"""
         r11 = Rectangle(10, 10)
         self.assertEqual(r11.area(), 100)
 
     def test_display_method(self):
+        """Rectangle display method unittest"""
         output = io.StringIO()
         sys.stdout = output
         r12 = Rectangle(2, 2)
@@ -65,14 +72,16 @@ class TestRectangleClass(unittest.TestCase):
         self.assertEqual(output.getvalue(), "##\n##\n")
 
     def test_str_method(self):
+        """Rectangle __str__ method unittest"""
         r13 = Rectangle(2, 2)
         str_r = r13.__str__()
-        self.assertEqual(str_r, '[Rectangle] (24) 0/0 - 2/2')
+        self.assertEqual(str_r, '[Rectangle] (25) 0/0 - 2/2')
         r14 = Rectangle(4, 6, 2, 1, 12)
         str_r1 = r14.__str__()
         self.assertEqual(str_r1, '[Rectangle] (12) 2/1 - 4/6')
 
     def test_display_method_w_coordinates(self):
+        """Rectangle display method unittest"""
         output = io.StringIO()
         sys.stdout = output
         r15 = Rectangle(2, 2, 2, 1)
@@ -81,6 +90,7 @@ class TestRectangleClass(unittest.TestCase):
         self.assertEqual(output.getvalue(), "\n  ##\n  ##\n")
 
     def test_update_method(self):
+        """Rectangle update method unittest"""
         r16 = Rectangle(2, 2)
         r16.update(3)
         self.assertEqual(r16.id, 3)
@@ -99,7 +109,7 @@ class TestRectangleClass(unittest.TestCase):
         r17 = Rectangle(1, 1)
         r17.update(width=1, x=2)
         str_r = r17.__str__()
-        self.assertEqual(str_r, '[Rectangle] (27) 2/0 - 1/1')
+        self.assertEqual(str_r, '[Rectangle] (28) 2/0 - 1/1')
         r18 = Rectangle(2, 2)
         r18.update(x=1, height=2, y=3, width=4, id=99)
         str_r1 = r18.__str__()
@@ -109,13 +119,13 @@ class TestRectangleClass(unittest.TestCase):
         self.assertEqual(str_r2, '[Rectangle] (10) 10/10 - 10/10')
 
     def test_to_dictionary_method(self):
+        """Rectangle to_dictionary method unittest"""
         r20 = Rectangle(3, 3)
         d = r20.to_dictionary()
         self.assertEqual(d['width'], 3)
         self.assertEqual(d['height'], 3)
         self.assertEqual(d['x'], 0)
         self.assertEqual(d['y'], 0)
-
 
 if __name__ == '__main__':
     unittest.main()
