f08277c303ed2b633ca6000b5cac4b6c311bf780
Omar <ocontreras.sf@gmail.com>
2018-06-26 02:00:17 +0000

    Capitalize from

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
 -- script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server
-DELETE from second_table WHERE score <= 5;
+DELETE FROM second_table WHERE score <= 5;
