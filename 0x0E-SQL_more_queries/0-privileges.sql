17b943d543fb32f9837abe86312d7f9c2d795df5
Omar <ocontreras.sf@gmail.com>
2018-06-26 18:55:20 +0000

    Add script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
--- 0x0E-SQL_more_queries/README.md
+++ 0x0E-SQL_more_queries/README.md
@@ -1,1 +1,2 @@
 # SQL - More queries
+* 0-privileges.sql: script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
