1b7336d979f2cb5614e4afe68cee27cf2af321dd
Omar <ocontreras.sf@gmail.com>
2018-06-29 05:09:23 +0000

    Alter first line

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- script that lists all shows from hbtn_0d_tvshows_rate by their rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) as rating FROM tv_shows
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
   INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
   GROUP BY tv_shows.title
   ORDER BY rating DESC;
