8ee80cbeaf1e364217a737feb546987f7caabd80
Omar <ocontreras.sf@gmail.com>
2018-07-20 23:17:36 +0000

    Add script that deletes all State objects with a name containing the letter a from the database

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Adds a new State object (Louisiana) to the database hbtn_0e_6_usa
+   takes arguments: mysql-username, mysql-password, database-name
+"""
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).filter(
+            State.name.ilike('%a%')).order_by(State.id):
+        session.delete(state)
+    session.commit()
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -12,3 +12,4 @@
 * 10-model_state_my_get.py: script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa
 * 11-model_state_insert.py: script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
 * 12-model_state_update_id_2.py: script that changes the name of a State object from the database hbtn_0e_6_usa
+* 13-model_state_delete_a.py: script that deletes all State objects with a name containing the letter a from the database
