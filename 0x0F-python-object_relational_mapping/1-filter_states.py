3cd46b59a34b71c7a1b058540c4627b630078e92
Omar <ocontreras.sf@gmail.com>
2018-07-20 03:24:58 +0000

    Add check for case-insensitive LIKE

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,10 +18,11 @@ if __name__ == '__main__':
             charset="utf8"
         )
         cur = conn.cursor()
-        cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'\
-        ORDER BY states.id ASC""")
+        cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'
+                    ORDER BY states.id ASC""")
         query_rows = cur.fetchall()
         for row in query_rows:
-            print(row)
+            if row[1][0] == "N":
+                print(row)
         cur.close()
         conn.close()
