0e61d99747a073807405e35303dd2d59acb45b9e
Omar <ocontreras.sf@gmail.com>
2018-07-20 20:16:13 +0000

    Uncomment password

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         host="localhost",
         port=3306,
         user=sys.argv[1],
-        #passwd=sys.argv[2],
+        passwd=sys.argv[2],
         db=sys.argv[3],
         charset="utf8"
     )
