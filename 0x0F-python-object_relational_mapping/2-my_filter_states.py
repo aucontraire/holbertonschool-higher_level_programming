5524757eb61e3f8d8bafb03261ae856c1ef0d5b2
Omar <ocontreras.sf@gmail.com>
2018-07-20 03:48:53 +0000

    Add check for match

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
     cur.execute(query.format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     conn.close()
