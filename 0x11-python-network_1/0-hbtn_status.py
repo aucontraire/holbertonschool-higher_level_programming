dd228fbdd3bac5a3ec5ce328d6084838ff89d4d4
Omar <ocontreras.sf@gmail.com>
2018-08-02 09:15:20 +0000

    Add code to make sure it does not execute when importing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,10 @@
 import urllib
 
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print('Body response:')
-    print('    - type: {}'.format(type(html)))
-    print('    - content: {}'.format(html))
-    print('    - utf8 content: {}'.format(html.decode('utf8')))
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('    - type: {}'.format(type(html)))
+        print('    - content: {}'.format(html))
+        print('    - utf8 content: {}'.format(html.decode('utf8')))
