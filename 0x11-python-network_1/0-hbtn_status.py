82f569ab3e4b6daea692547ee25e743e1c70cd6c
Omar <ocontreras.sf@gmail.com>
2018-08-02 09:16:40 +0000

    Use tab instead of spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
-        print('    - type: {}'.format(type(html)))
-        print('    - content: {}'.format(html))
-        print('    - utf8 content: {}'.format(html.decode('utf8')))
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf8')))
