f6510b66b62a760cd76b951afc8486801b049c2c
Omar <ocontreras.sf@gmail.com>
2018-08-02 09:45:41 +0000

    Add script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response """
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers['X-Request-Id'])
--- 0x11-python-network_1/README.md
+++ 0x11-python-network_1/README.md
@@ -1,2 +1,3 @@
 # Python - Network #1
 * 0-hbtn_status.py: script that fetches https://intranet.hbtn.io/status
+* 1-hbtn_header.py: script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response
