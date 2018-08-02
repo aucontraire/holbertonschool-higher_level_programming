87870135b375a85c22584b270fe8023c4c66dc6e
Omar <ocontreras.sf@gmail.com>
2018-08-02 09:46:41 +0000

    Decrease docstring width

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response """
+""" script that takes in a URL, sends a request to the URL and displays the
+    value of the X-Request-Id variable found in the header of the response
+"""
 if __name__ == '__main__':
     import sys
     import urllib.request
