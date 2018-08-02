adbe1052a983c387607c6f45be449233743ad2e3
Omar <ocontreras.sf@gmail.com>
2018-08-02 09:27:06 +0000

    Add single quotes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-""" script that fetches https://intranet.hbtn.io/status """
-import urllib
-
-
+""" script fetches https://intranet.hbtn.io/status"""
+import urllib.request
 if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode(encoding='utf8')))
+        print('\t- utf8 content: {}'.format(html.decode(encoding='utf-8')))
