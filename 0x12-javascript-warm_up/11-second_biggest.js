832efe0571c0f1a093cf4c6e31e7a57174c2b65c
Omar <ocontreras.sf@gmail.com>
2018-08-21 16:55:31 +0000

    Change second starting point

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ if (process.argv[2] === undefined || process.argv[3] === undefined) {
   console.log(0);
 } else {
   let fmax = parseInt(process.argv[2]);
-  let nmax = parseInt(process.argv[2]);
+  let nmax = parseInt(process.argv[3]);
   process.argv.forEach((val, index) => {
     if (index > 1) {
       if (fmax < parseInt(val)) {
