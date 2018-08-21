0294e4ae23d8ebaf728209aa09fef6b6ab804064
Omar <ocontreras.sf@gmail.com>
2018-08-21 14:56:29 +0000

    Remove variables

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-let a = parseInt(process.argv[2]);
-let b = parseInt(process.argv[3]);
-
 function add(a, b) {
   return a + b;
 }
 
-console.log(add(a, b));
+console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
