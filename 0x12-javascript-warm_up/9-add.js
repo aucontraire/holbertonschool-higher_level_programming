de232cf2856a3a0f6935063cbe1662502d8d76db
Omar <ocontreras.sf@gmail.com>
2018-08-21 15:02:03 +0000

    Try a different approach

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-function add(a, b) {
-  return a + b;
+function add (a, b) {
+  return parseInt(a) + parseInt(b);
 }
-
-console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
+console.log(add(process.argv[2], process.argv[3]));
