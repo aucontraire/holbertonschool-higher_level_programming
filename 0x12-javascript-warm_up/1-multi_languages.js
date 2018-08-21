28b891bc61d11583f6218c3b3295952b52358f06
Omar <ocontreras.sf@gmail.com>
2018-08-21 11:26:36 +0000

    Change curly braces to JavaScript standard instead of C

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,6 @@
 let i = 0;
 const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (i = 0; i < arr.length; i++)
-{
+for (i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
