3f4c50524c6e3578a25b875b6fd4e5060d595975
Omar <ocontreras.sf@gmail.com>
2018-08-21 11:59:57 +0000

    Use the triple equal sign convention

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv.length < 3) {
   console.log('No argument');
-} else if (process.argv.length == 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
