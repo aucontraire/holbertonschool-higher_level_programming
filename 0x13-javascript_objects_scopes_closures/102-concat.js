22e1320eecf8d3407d97832fcf762bfd827e304a
Omar <ocontreras.sf@gmail.com>
2018-09-03 14:42:37 +0000

    Use let instead of var

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const fileA = fs.readFileSync(process.argv[2]);
 const fileB = fs.readFileSync(process.argv[3]);
 const arr = [fileA, fileB];
-var file = fs.createWriteStream(process.argv[4]);
+let file = fs.createWriteStream(process.argv[4]);
 arr.forEach(function (line) {
   file.write(line + '\n');
 });
