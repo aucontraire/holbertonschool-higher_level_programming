e1713f8bc320d99df96f4eb47b36d11d7617d824
Omar <ocontreras.sf@gmail.com>
2018-09-05 23:36:39 +0000

    Add script that computes the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const options = {
+  url: process.argv[2],
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    let results = {};
+    let i = 0;
+    for (i = 0; i < data.length; i++) {
+      if (!(data[i]['userId'] in results)) {
+        results[data[i]['userId']] = 0;
+      }
+      if (data[i]['completed']) {
+        results[data[i]['userId']] += 1;
+      }
+    }
+    console.log(results);
+  }
+}
+
+request(options, callback);
--- 0x14-javascript-web_scraping/README.md
+++ 0x14-javascript-web_scraping/README.md
@@ -5,3 +5,4 @@
 * 3-starwars_title.js: script that prints the title of a Star Wars movie where the episode number matches a given integer
 * 4-starwars_count.js: script that prints the number of movies where the character “Wedge Antilles” is present
 * 5-request_store.js: script that gets the contents of a webpage and stores it in a file
+* 6-completed_tasks.js: script that computes the number of tasks completed by user id
