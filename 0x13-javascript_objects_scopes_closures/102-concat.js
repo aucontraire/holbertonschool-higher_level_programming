f69e551e42436dff249e2064047becd3f05dd3f7
Omar <ocontreras.sf@gmail.com>
2018-09-06 03:10:13 +0000

    Change to appendfile

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,7 +5,11 @@ const fs = require('fs');
 const fileA = fs.readFileSync(process.argv[2]);
 const fileB = fs.readFileSync(process.argv[3]);
 const arr = [fileA, fileB];
-let file = fs.createWriteStream(process.argv[4]);
-arr.forEach(function (line) {
-  file.write(line + '\n');
-});
+
+for (let i in arr) {
+  fs.appendFile('fileC', arr[i] + '\n', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
